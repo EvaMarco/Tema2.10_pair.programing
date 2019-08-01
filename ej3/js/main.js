397ef2e1510511acb0052e5cf191c3543379e79a
ClaBri <clarabrigido@gmail.com>
2019-08-01 10:08:06 +0200

    add ejer3

--- ej3/js/main.js
+++ ej3/js/main.js
@@ -2,13 +2,21 @@
 
 const input = document.querySelector('.input');
 const button = document.querySelector('.btn');
-
-const ENDPOINT = `https://api.github.com/users/${input.value}`;
+const name = document.querySelector('.name');
+const avatar = document.querySelector('.imagen');
+const repo = document.querySelector('.repo');
 
 function call() {
+    const ENDPOINT = `https://api.github.com/users/${input.value}`;
     fetch(ENDPOINT)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
+            name.innerHTML = data.name;
+            avatar.src = data.avatar_url;
+            repo.innerHTML = data.public_repos;
+        })
+
 };
 
 button.addEventListener('click', call);
